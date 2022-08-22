6d7057723a719e45643d61d25acfe74c6beb8b42
amolpandit38 <amol38.ap@gmail.com>
2022-08-22 19:09:19 +0530

    about reverting and resetting

--- hungry.py
+++ hungry.py
@@ -5,3 +5,10 @@ print('I used "git checkout -- hungry.py to undo the '
       'uncommitted changes in my code. now pushing it after undoing the line"')
 print("to undo all the files "
       "edited with uncommitted changes then used 'git checkout -- .'")
+print("to revert changes 'git revert <commit id>'. you will get commit id by 'git log' command."
+      "by reverting you mean that you have to undo the previous commit you made."
+      "'git revert -n <commit id>' does the reverting process but does not commit the undoing its "
+      "status remains modified")
+print("while revert command helps in reverting the previous commit, resetting helps multiple commits"
+      "serially to revert, 'git reset --hard <commit id>' commit id must be the id from where the above "
+      "commits, seem useless")
