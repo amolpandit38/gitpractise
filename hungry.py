225c9bfb0dc65e3e055a2459434dcab2e1fcba9b
amolpandit38 <amol38.ap@gmail.com>
2022-08-22 15:14:04 +0530

    used . to undo all scripts

--- hungry.py
+++ hungry.py
@@ -3,3 +3,5 @@ print("I'm Amol")
 print("I'm not hungry")
 print('I used "git checkout -- hungry.py to undo the '
       'uncommitted changes in my code. now pushing it after undoing the line"')
+print("to undo all the files "
+      "edited with uncommitted changes then used 'git checkout -- .'")
