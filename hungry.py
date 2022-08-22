c0977315903db09012c44306025f9e5a10f5211b
amolpandit38 <amol38.ap@gmail.com>
2022-08-22 15:06:02 +0530

    used checkout command

--- hungry.py
+++ hungry.py
@@ -1,3 +1,5 @@
 print("I'm hungry")
 print("I'm Amol")
 print("I'm not hungry")
+print('I used "git checkout -- hungry.py to undo the '
+      'uncommitted changes in my code. now pushing it after undoing the line"')
